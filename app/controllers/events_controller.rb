b8cf91315d41d2c707c840e45b3c3e7e0c73ed33
Dustin Coates <dustin@dcoates.com>
2012-12-08 15:35:54 -0500

    Add event _form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,10 +2,20 @@ class EventsController < ApplicationController
   def show
   end
   def new
-
+    @event = Event.new
+    @map = Map.find(params[:map_id])
+    @user = User.find(params[:user_id])
+    @existing_events = Event.where(:map_id => @map.id)
   end
   def create
-
+    map = Map.find(params[:map_id])
+    event = Event.new(params[:event])
+    event.map_id = map.id
+    if event.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
   def edit
 
